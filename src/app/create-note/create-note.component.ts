6c18e8b752f80fb2f93e000c73ab76a5fe3f5680
Ariana Faraday <ariana@thinkful.com>
2018-07-09 17:50:52 -0400

    adds mock data, and sets up HTML

--- src/app/create-note/create-note.component.ts
+++ src/app/create-note/create-note.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Note } from "../models";
+import { NOTES } from "../mock-data";
 
 @Component({
   selector: 'app-create-note',
@@ -6,6 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-note.component.css']
 })
 export class CreateNoteComponent implements OnInit {
+
+  //Create a new, epmty note, which is referenced in our form
+  note = new Note();
+  //Declare that the form has not been submitted yet.
+  submitted = false;
+  //Import mock data to the notes list.
+  _notes = NOTES;
+
+  submitForm = form => {
+    //Declare that the form has been submitted.
+    this.submitted = true;
+    const update_notes = this._notes;
+    //Add notes submitted through the form to the list of notes displayed on the page.
+    update_notes.push({
+      id: 0,
+      title: form.value.title,
+      content: form.value.content
+    });
+    this._notes = update_notes;
+  }
 
   constructor() { }
 
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Note } from '../models'
 
 @Component({
@@ -6,13 +6,10 @@ import { Note } from '../models'
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css']
 })
+
 export class NotesComponent implements OnInit {
 
-  note : Note = {
-    id: 1,
-    title: "Shopping List",
-    content: "milk, butter, ice cream, strawberries, apples, grapes, jelly"
-  }
+  @Input() _notes;
   
   constructor() { }
 
