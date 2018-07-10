8f9dd939a73a8c03454a9ba89155cc9686c25eeb
Ariana Faraday <ariana@thinkful.com>
2018-07-09 20:15:34 -0400

    updates components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
 
 import { NotesService } from './_services/notes.service';
+import { BaseService } from './_services/base.service';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
-import { CreateNoteComponent } from './create-note/create-note.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
-    CreateNoteComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [NotesService],
+  providers: [
+    NotesService,
+  BaseService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -18,8 +18,51 @@ export class NotesComponent implements OnInit {
     this.notesService.getNotes()
       .subscribe(notes => this._notes = notes);
   }
+  note = new Note();
+  notes: Note[];
+
+  submitted = false;
+
+  submitForm(noteForm) {
+    this.submitted = true;
+    console.log('here is the note', noteForm);
+    const title = noteForm.value.title,
+      content = noteForm.value.content
+  
+    let method;
+  
+    if (this.note.id) {
+      method = this.notesService.updateNote({ title, content, id: this.note.id } as Note);
+    } else {
+      method = this.notesService.addNote({ title, content } as Note);
+    }
+  
+    method.subscribe(note => {
+      this.note = new Note();
+      this.getNotes();
+    });
+  }
+
+  updateNote(note) {
+    this.note = {
+      ...note,
+      tags: note.tags.map(t => t.id)
+    };
+  }
+  
+  cancelUpdate() {
+    this.note = new Note();
+  }
+
+  deleteNote(id, e): void {
+    if (confirm('Are you sure want to delete this note?')) {
+      this.notesService.deleteNote(id).subscribe(() => console.log('user deleted'));
+      this.getNotes();
+    }
+  }  
   
   ngOnInit() {
+    this.getNotes();
   }
 
 }
